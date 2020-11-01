4d001f5ba383b88a9aa60e5ebeebbd026bd22444
Ronny <mwithiamoki@gmail.com>
2020-11-02 00:47:08 +0300

    added class profile

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -19,3 +19,17 @@ class Neighbourhood(models.Model):
     def search_by_name(cls,search_term):
         neighborhood=cls.objects.filter(name__icontains=search_term)
         return neighborhood
+
+class Profile(models.Model):
+    image=models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio=models.CharField(max_length=300)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
