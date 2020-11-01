356b341e8e0bbb3f632574c07e14e5e34022bfab
Ronny <mwithiamoki@gmail.com>
2020-11-02 00:39:08 +0300

    defined home and neighbourhood

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Neighbourhood, Profile
+from .forms import *
+from django.views import generic
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    neighbourhoods = Neighbourhood.objects.all()
+    return render(request, 'home.html',{"neighbourhoods":neighbourhoods,})
+
+
+@login_required(login_url='/accounts/login/')
+def neighbourhood(request):
+    neighbourhoods = Neighbourhood.objects.all()
+    return render(request,'home.html',{"neighbourhoods":neighbourhoods})
