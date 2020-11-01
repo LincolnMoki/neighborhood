3af12643c41bd26a92540fd96ec3ee23efeca379
Ronny <mwithiamoki@gmail.com>
2020-11-02 00:46:39 +0300

    added class neighbourhoods

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Neighbourhood(models.Model):
+    name=models.CharField(max_length=60)
+    location=models.CharField(max_length=60)
+    population=models.IntegerField()
+    image = models.ImageField(upload_to = 'images/')
+
+    def create_neigborhood(self):
+        self.save()
+
+    def delete_neigborhood(self):
+        self.delete()
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        neighborhood=cls.objects.filter(name__icontains=search_term)
+        return neighborhood
